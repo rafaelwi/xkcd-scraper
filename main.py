9c2e8bf9d087023ad1f418f2c3aa6fe878b794ac
rafaelwi <rafwiskailnicki@gmail.com>
2019-04-24 13:34:25 -0400

    Added command line args

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 import urllib.request
+import sys
 
 """
 get_page(url): Gets the page source of a requested website
@@ -46,14 +47,34 @@ def log_error(e):
     print(e)
 
 """
+log_message(m): Prints out the message passed in. Used for debugging
+In
+    m: A message
+Out: The message
+"""
+def log_message(m):
+    print (m)
+
+"""
     Main Program
 """
 searchStr = 'Image URL'
-raw_url = 'https://xkcd.com/2139/'
+
+# Check if the number of args entered is correct
+if len(sys.argv) != 2:
+    print ("Usage: python3 main.py <xkcd url>")
+    sys.exit()
+else: 
+    raw_url = sys.argv[1]
+    log_message ("Got URL: " + raw_url)
+
+    # TODO: Validate that a URL entered is in the valid format of
+    # "https://xkcd.com/wxyz/"
 
 # Get the page and place into Beautiful Soup Object
 raw_html = get_page(raw_url)
 bs4_html = BeautifulSoup(raw_html, 'html.parser')
+log_message ("Got page")
 
 # Get only the text from raw_html and then find the image URL
 bs4_text = bs4_html.get_text()
@@ -62,8 +83,10 @@ bs4_text = bs4_text[start:]
 bs4_text_list = bs4_text.splitlines() # index 0 will be the line containing the image URL
 img_url_line = bs4_text_list[0]
 img_url = img_url_line.split(' ')[4] # now have the image url
+
+# Create the filename
 filename = "imgs/" + raw_url.split('/')[3] + ".png"
 
 # Download the image
 urllib.request.urlretrieve(img_url, filename)
-#eop
+log_message ("Saved image from URL <" + raw_url + "> as " + filename)
