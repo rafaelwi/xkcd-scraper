d5eec3cdf4b786a575a4b67383e17600af721ca5
rafaelwi <rafwiskailnicki@gmail.com>
2019-04-24 13:06:49 -0400

    Added URL retreval

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
+import urllib.request
 
 """
 get_page(url): Gets the page source of a requested website
@@ -58,5 +59,6 @@ bs4_text = bs4_html.get_text()
 start = bs4_text.index(searchStr)
 bs4_text = bs4_text[start:]
 bs4_text_list = bs4_text.splitlines() # index 0 will be the line containing the image URL
-print (bs4_text_list[0])
+img_url_line = bs4_text_list[0]
+img_url = img_url_line.split(' ')[4] # now have the image url
 #eop
