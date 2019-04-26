70a30eb87b8b965f9f486e3fc73458c1456846ad
rafaelwi <rafwiskailnicki@gmail.com>
2019-04-26 17:21:01 -0400

    randomcomic now downloads a random comic

--- randomcomic.py
+++ randomcomic.py
@@ -3,6 +3,7 @@
 
 # Imports
 import sys
+import random
 import functs as xkcd
 
 """
@@ -11,3 +12,12 @@ import functs as xkcd
 # Get the latest xkcd value
 latest = xkcd.get_latest()
 
+# Randomly generate a number
+random_comic = random.randint(1, int(latest))
+random_comic_url = "https://xkcd.com/" + str(random_comic) + "/"
+
+# Get image URL
+img_url = xkcd.get_img_url(random_comic_url)
+
+# Download image URL
+xkcd.download_img(random_comic_url, img_url)
